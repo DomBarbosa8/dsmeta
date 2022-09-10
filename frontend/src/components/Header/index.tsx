8aae200a380e169cc37e350f6bbdc0f304437298
Dominic Barbosa <dom.freedom88@gmail.com>
2022-09-10 20:11:37 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,15 +1,15 @@
-import logo from '../../assets/img/logo.svg'
-import './styles.css'
+import logo from '../../assets/img/logo.svg';
+import './styles.css';
 
 function Header() {
   return (
     <header>
-      <div class="dsmeta-logo-container">
+      <div className="dsmeta-logo-container">
         <img src={logo} alt="DSMeta" />
         <h1>DSMeta</h1>
         <p>
           Desenvolvido por
-          <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+          <a href="https://www.instagram.com/devsuperior.ig">@dom</a>
         </p>
       </div>
     </header>
